76d323049e383fd35bdc78efefd58e614ab9253c
dimaboi2101 <dimaboi21@gmail.com>
2022-11-13 11:32:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("hello world")
+
+// 1- string "строка.рядок"
+// 2- number 4 5 6  номер
+// 3- boolean (true \ false) так або ні
+// 4- null нічого
+// 5- indefined щось не визначено
+// 6- symbol
+// 7- bigint -числовий тип дпнних
+//
+// object = {}
+// Array = []
+
+// Змінні ----
+
+// var string = "Стрічка!"; -- старий спосіб, в сучасній розробці використовується дуже рідко,
+// має глобальну область видимості
+
+// let number = 333;
+
+// const name = "Vasilisa";
+
+// Наша задача
+// знайти елименти
+// відловити клік, прослухати клік
+// взаємодія з кліком
+//
+
+const menuBtn = document.querySelector(".menu");
+
+const menuItem = document.querySelector(".nav__list");
+
+menuBtn.addEventListener("click", () => {
+    // menuItem,classlist.add("active");
+    // menuItem.classList.remove("active");
+    menuItem.classList.toggle("active");
+    menuBtn.classList.toggle("active__btn")
+});
+
+
+//   menuItem.classList.add("active"); --тільки додає клас
+// menuItem.classList.remuve("active"); -- тільки забирае класс
+//
